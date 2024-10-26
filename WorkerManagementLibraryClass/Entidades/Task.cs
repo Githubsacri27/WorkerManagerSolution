5030a73a08b22b64f3f406c9c8c86d9826b00f5e
Githubsacri27 <rsacri27@hotmail.com>
2024-10-26 21:21:49 +0200

    añadir propiedades a task

--- WorkerManagementLibraryClass/Entidades/Task.cs
+++ WorkerManagementLibraryClass/Entidades/Task.cs
@@ -8,5 +8,19 @@ namespace WorkerManagementLibraryClass.Entidades
 {
     public class Task
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Technology { get; set; }
+        public string Status { get; set; }
+        public int IdWorker { get; set; }
+
+        public Task (int id, string description, string technology, string status, int idworker)
+        {
+            this.Id = id;
+            this.Description = description;
+            this.Technology = technology;
+            this.Status = status;
+            this.IdWorker = idworker;
+        }
     }
 }
--- WorkerManagementLibraryClass/Metodos/TaskManager.cs
+++ WorkerManagementLibraryClass/Metodos/TaskManager.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkerManagementLibraryClass.Entidades;
 
 namespace WorkerManagementLibraryClass.Metodos
 {
     public class TaskManager
     {
-        
+        private List<TaskManager> tasks = new List<TaskManager>();
+
+
+        // TODO: Método para crear un Task
+        //public void CreateTask()
+        //{
+        //    Console.WriteLine("Introduce the team name:");
+        //    string taskName = Console.ReadLine().Trim();
+
+        //    var task = new TaskManager { Id = taskName };
+        //    task.Add(task);
+        //    Console.WriteLine($"New task {task.Name}created successfully.");
+
+        //}
+
     }
 }
